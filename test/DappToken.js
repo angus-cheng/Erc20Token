4774a9a15fc9fa191b4471972f9cf976da74195f
Angus Cheng <anguscheng389@gmail.com>
2021-12-02 18:50:40 +1000

    Added testing for token ownership transfer

--- test/DappToken.js
+++ test/DappToken.js
@@ -5,7 +5,7 @@ let DappToken = artifacts.require("./DappToken.sol");
 contract("DappToken", function(accounts) {
     var tokenInstance;
 
-    it("Initializes the contract witht the correct values", function() { 
+    it("Initializes the contract with the correct values", function() { 
         return DappToken.deployed().then(function(instance) {
             tokenInstance = instance;
             return tokenInstance.name();
@@ -33,11 +33,21 @@ contract("DappToken", function(accounts) {
         return DappToken.deployed().then(function(instance) {
             tokenInstance = instance;
             // Test "require" statement first by transferring something larger than the sender's balance
-            return tokenInstance.transfer.call(accounts[1], Number.MAX_SAFE_INTEGER);   // call does not trigger transaction - used to inspect event
+            // Call does not trigger transaction - used to inspect event and whether it returns true/false
+            return tokenInstance.transfer.call(accounts[1], Number.MAX_SAFE_INTEGER);           
         }).then(assert.fail).catch(function(error) {
             assert(error.message, "Error message must contain revert");
+            // Only blockchain recipt/log can be inspected from transfer
+            return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0]});
+        }).then(function(success) {
+            assert.equal(success, true, "Transfer returns true");
             return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
         }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, "Triggers one event");
+            assert.equal(receipt.logs[0].event, "Transfer", "'Transfer' event shouold exist");
+            assert.equal(receipt.logs[0].args._from, accounts[0], "Logs the account the tokens are transferred from");
+            assert.equal(receipt.logs[0].args._to, accounts[1], "Logs the account the tokens are transferred to");
+            assert.equal(receipt.logs[0].args._value, 250000, "Logs the transfer amount");
             return tokenInstance.balanceOf(accounts[1]);
         }).then(function(balance) {
             assert.equal(balance.toNumber(), 250000), "Adds the amount to the receiving account";
